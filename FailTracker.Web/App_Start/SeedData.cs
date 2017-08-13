ec7bc5bc5f2b8aed9ec789b2f8789bde1221e999
Akachai <AkachaiPC@DESKTOP-7KMMA7G>
2017-08-13 15:40:45 +0700

    Add seed data using task (at start up)

---
+++ FailTracker.Web/App_Start/SeedData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FailTracker.Web.Infrastructure.Tasks;
+using FailTracker.Web.Models;
+
+namespace FailTracker.Web
+{
+    public class SeedData : IRunAtStartup
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SeedData(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void Execute()
+        {
+            if (!_context.Users.Any())
+            {
+                _context.Users.Add(new ApplicationUser
+                {
+                    UserName = "TestUser"
+                });
+
+                _context.SaveChanges();
+            }
+            if (!_context.Issues.Any())
+            {
+                var user = _context.Users.First();
+
+                _context.Issues.Add(new Issue(user, "Test Issue 1", "Test Issue Body 1"));
+                _context.Issues.Add(new Issue(user, "Test Issue 2", "Test Issue Body 2"));
+                _context.Issues.Add(new Issue(user, "Test Issue 3", "Test Issue Body 3"));
+
+                _context.SaveChanges();
+            }
+
+        }
+    }
+}
